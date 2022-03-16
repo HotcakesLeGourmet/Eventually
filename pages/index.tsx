30418737df457b333eb65784900e0e3ff977230a
HotcakesLeGourmet <gabrielmartinez@gmail.com>
2022-03-16 17:25:02 -0500

    Tailwind custom styles and colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
     return (
-        <div>
-            <h1 className="text-sm font-bold underline">Hello world!</h1>
-        </div>
+        <>
+            <div className="flex flex-col h-screen justify-center">
+                <div className="max-w-full flex justify-center">
+                    <div className="grid gap-4 m-4 w-1/2 grid-cols-3 grid-rows-2 text-center">
+                        <h1 className="bg-primary-work_theme activities-text">Work</h1>
+                        <h1 className="bg-primary-play_theme activities-text">Play</h1>
+                        <h1 className="bg-primary-study_theme activities-text">Study</h1>
+                        <h1 className="bg-primary-exercise_theme activities-text">Exercise</h1>
+                        <h1 className="bg-primary-social_theme activities-text">Social</h1>
+                        <h1 className="bg-primary-self_care_theme activities-text">Self-care</h1>
+                    </div>
+                </div>
+                <div className="flex max-w-full justify-center m-8">
+                    <div className=" w-10/12 grid grid-cols-2 grid-rows-2 h-80 gap-2">
+                        <div className="bg-neutral-pale_blue rounded-2xl text-center"></div>
+                        <div className="bg-neutral-desaturated_blue rounded-2xl"></div>
+                        <div className="bg-neutral-dark_blue rounded-2xl"></div>
+                        <div className="bg-neutral-very_dark_blue rounded-2xl"></div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
+function withOpacityValue(variable) {
+    return ({ opacityValue }) => {
+        if (opacityValue === undefined) {
+            return `rgb(var(${variable}))`;
+        }
+        return `rgb(var(${variable}) / ${opacityValue})`;
+    };
+}
 module.exports = {
     content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    work_theme: withOpacityValue("--primary-Light-red-work"),
+                    play_theme: withOpacityValue("--primary-Soft-blue-play"),
+                    study_theme: withOpacityValue("--primary-Light-red-study"),
+                    exercise_theme: withOpacityValue("--primary-Lime-green-exercise"),
+                    social_theme: withOpacityValue("--primary-Violet-social"),
+                    self_care_theme: withOpacityValue("--primary-Soft-orange-self-care"),
+                },
+                neutral: {
+                    very_dark_blue: withOpacityValue("--neutral-Very-dark-blue"),
+                    dark_blue: withOpacityValue("--neutral-Dark-blue"),
+                    desaturated_blue: withOpacityValue("--neutral-Desaturated-blue"),
+                    pale_blue: withOpacityValue("--neutral-Pale-Blue"),
+                },
+            },
+        },
     },
     plugins: [],
 };
